5b4068cbbe6490f6eb068b56726c97f9b127d930
Mickael GOETZ <contact@mickael-goetz.com>
2024-11-06 12:33:38 +0100

    add debug info about openssl version, php version and previous exception code

--- test.php
+++ test.php
@@ -12,6 +12,11 @@ $directory = __DIR__;
 $source = $directory.DIRECTORY_SEPARATOR.'encrypted.pdf';
 $destination = $directory.DIRECTORY_SEPARATOR.'generated.pdf';
 
+echo 'PHP_VERSION: ' . PHP_VERSION . "\n";
+echo 'OPENSSL_VERSION_NUMBER: '.OPENSSL_VERSION_NUMBER."\n";
+echo 'OPENSSL_VERSION_TEXT: '.OPENSSL_VERSION_TEXT."\n";
+echo "\n";
+
 try {
     echo "Opening file $source\n";
     $pdf->setSourceFileWithParserParams($source, [PdfParser::PARAM_IGNORE_PERMISSIONS => true]);
@@ -23,6 +28,20 @@ try {
     echo "✅ The PDF has been processed\”";
 } catch (\Exception $exception) {
     echo sprintf("%s with code %d : %s\n", get_class($exception), $exception->getCode(), $exception->getMessage());
+    $prev = $exception->getPrevious();
+    assert($prev !== null);
+
+    $class = new ReflectionClass(get_class($prev));
+    $constants = $class->getConstants();
+    $constantName = null;
+    foreach ($constants as $name => $value) {
+        if ($value === $prev->getCode()) {
+            $constantName = $name;
+            break;
+        }
+    }
+
+    echo sprintf("PREVIOUS: %s with code %d (%s) : %s\n", $class->name, $prev->getCode(), $constantName ?? 'unknown', $prev->getMessage());
     echo "❌ The PDF could not be processed\n";
 }
 
